bc16e3a9eccb97e5a9619ead02f1baf3c0420a0f
ilbaltin <0979006668@ukr.net>
2022-04-08 21:52:38 +0300

    Initial commit

---
+++ src/test/java/ua/soundmag/SearchTests.java
@@ -0,0 +1,26 @@
+package ua.soundmag;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class SearchTests {
+
+    @Test
+    void searchProductByTitle(){
+        Selenide.open("https://brain.com.ua/ukr/");
+
+        var productName = "SENNHEISER CX 400BT TRUE WIRELESS BLACK";
+
+        $(".header-bottom-in").$(".quick-search-input").setValue(productName).click();
+        $(".qsr-form").$(".qsr-submit").click();
+        $(".br-static").$(".add.br-bbb-f").click();
+        $(By.xpath("//*[@id=\"checkout-modal-popup\"]/div/div[1]/div/div[1]/p/span/button")).click();
+
+       Selenide.sleep(4000);
+
+    }
+
+}
